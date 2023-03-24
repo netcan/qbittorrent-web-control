76e1bf6a7741a40958e7d4d3a3486b5631bd5279
netcan <netcan1996@gmail.com>
2023-03-24 21:24:53 +0800

    add progress info

--- app.py
+++ app.py
@@ -1,62 +1,56 @@
-from flask import Flask, request, render_template
-from flask_bootstrap import Bootstrap
-import requests,os
-import json
+import os
+import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
-# qbittorrent API相关配置
-QB_HOST = "http://intel-n100.local:8080"
+QB_BASE_URL = "http://intel-n100.local:8080"
 QB_USERNAME = os.environ.get('QB_USERNAME')
 QB_PASSWORD = os.environ.get('QB_PASSWORD')
 
-# 获取qbittorrent API授权
-def get_auth_cookie():
-    data = {'username': QB_USERNAME, 'password': QB_PASSWORD}
-    response = requests.post(QB_HOST + '/api/v2/auth/login', data=data)
-    auth_cookie = None
-    if response.status_code == 200:
-        headers = response.headers
-        if 'Set-Cookie' in headers:
-            auth_cookie = headers['Set-Cookie'].split('SID=')[1].split(';')[0]
-    else:
-        print("Failed to get auth token. Response code: ", response.status_code)
-        print(response.text)
-    return auth_cookie
 
-# 添加下载任务
-def add_torrent(url, auth_cookie):
-    headers = {
-        'Cookie': f"SID={auth_cookie}",
-    }
-    data = {'urls': url}
-    response = requests.post(QB_HOST + '/api/v2/torrents/add', headers=headers, data=data)
-    return response.status_code
+def get_sid():
+    login_url = QB_BASE_URL + "/api/v2/auth/login"
+    data = {"username": QB_USERNAME, "password": QB_PASSWORD}
+    response = requests.post(login_url, data=data)
+    cookie = response.cookies.get("SID")
+    return {"SID": cookie}
 
-# 获取下载任务列表
-def get_torrent_list(auth_cookie):
-    headers = {
-        'Cookie': f"SID={auth_cookie}",
-    }
-    response = requests.get(QB_HOST + '/api/v2/torrents/info', headers=headers)
-    json_data = json.loads(response.content.decode('utf-8'))
-    return json_data
 
-@app.route('/', methods=['GET', 'POST'])
+def torrents_info():
+    url = QB_BASE_URL + "/api/v2/torrents/info"
+    response = requests.get(url, cookies=get_sid())
+    torrents = response.json()
+    return torrents
+
+
+def get_torrents(torrents):
+    downloading_torrents = []
+    completed_torrents = []
+
+    for torrent in torrents:
+        if torrent['state'] == 'downloading':
+            downloading_torrents.append(torrent)
+        elif torrent['state'] == 'pausedUP' and torrent['progress'] == 1:
+            completed_torrents.append(torrent)
+
+    return downloading_torrents, completed_torrents
+
+@app.route("/")
 def index():
-    auth_cookie = get_auth_cookie()
-    if request.method == 'POST':
-        url = request.form['url']
-        status_code = add_torrent(url, auth_cookie)
-        if status_code == 200:
-            message = "Torrent added successfully."
-        else:
-            message = "Failed to add torrent."
-        return render_template('index.html', message=message)
-    else:
-        torrent_list = get_torrent_list(auth_cookie)
-        return render_template('index.html', torrent_list=torrent_list)
+    torrents = torrents_info()
+    downloading_files = []
+    downloaded_files = []
 
-if __name__ == '__main__':
+    for torrent in torrents:
+        progress = torrent['progress']
+        if progress < 1:
+            downloading_files.append(torrent)
+        else:
+            downloaded_files.append(torrent)
+
+    return render_template('index.html', downloading_files=downloading_files, downloaded_files=downloaded_files)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
