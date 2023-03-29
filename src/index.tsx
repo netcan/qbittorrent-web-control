e30b0753e89912bfb503b340217f73bedf07431d
netcan <netcan1996@gmail.com>
2023-03-29 21:01:30 +0800

    add torrent interface

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { StyledEngineProvider } from '@mui/material/styles';
 import reportWebVitals from './reportWebVitals';
+import { NextUIProvider } from '@nextui-org/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <StyledEngineProvider injectFirst>
+      <NextUIProvider>
           <App />
-      </StyledEngineProvider>
+      </NextUIProvider>
   </React.StrictMode>
 );
 
